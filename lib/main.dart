169f15a0a054f3c4ab19489e9b5002d9d31f8492
NdegwaJM <juliusndegwa2@gmail.com>
2022-01-24 10:36:57 +0300

    Using date format package intl

--- lib/main.dart
+++ lib/main.dart
@@ -21,13 +21,13 @@ class MyHomePage extends StatelessWidget {
     Transaction(
       id: 't1',
       title: 'New shoes',
-      amount: 67.99,
+      amount: 17.99,
       date: DateTime.now(),
     ),
     Transaction(
       id: 't2',
-      title: 'New Laptop',
-      amount: 657.99,
+      title: 'Weekly Groceries',
+      amount: 57.99,
       date: DateTime.now(),
     ),
   ];
@@ -35,26 +35,70 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Expense Plannner'),
+        title: Text('Flutter App'),
       ),
       body: Column(
-        // ignore: prefer_const_literals_to_create_immutables
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.center,
-
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
             width: double.infinity,
             child: Card(
-              color: Colors.green,
-              child: Text('CHART !'),
+              color: Colors.blue,
+              child: Text('CHART!'),
               elevation: 5,
             ),
           ),
           Column(
             children: transactions.map((tx) {
               return Card(
-                child: Text(tx.title),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.purple,
+                          width: 2,
+                        ),
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        // adding a $ sign with string interpolation
+                        //
+                        //'\$${tx.amount}',
+                        'Ksh ' + tx.amount.toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple[900],
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          tx.title,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.red[700],
+                          ),
+                        ),
+                        Text(
+                          tx.date.toString(),
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               );
             }).toList(),
           ),
